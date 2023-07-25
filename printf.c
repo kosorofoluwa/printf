0fd0c2a592f3edb3a250204234e71f8960c9644b
Niyi <akinwaleadeniyi2@gmail.com>
2023-07-25 16:34:00 -0700

    modification

--- printf.c
+++ printf.c
@@ -12,9 +12,7 @@ int _printf(const char *format, ...)
 	va_list arg_list;
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
 
 	va_start(arg_list, format);
 	while (*format)
@@ -27,9 +25,8 @@ int _printf(const char *format, ...)
 				char c = va_arg(arg_list, int);
 
 				write(1, &c, 1);
-				print_char++;
 			}
-			if (*format == 's')
+			else if (*format == 's')
 			{
 				char *str = va_arg(arg_list, char*);
 				int str_len = 0;
@@ -37,25 +34,15 @@ int _printf(const char *format, ...)
 				while (str[str_len] != '\0')
 					str_len++;
 				write(1, str, str_len);
-				print_char++;
-			}
-			else if (*format == '\0')
-			{
-				break;
 			}
 			else if (*format == '%')
-			{
 				write(1, format, 1);
-				print_char++;
-			}
 		}
-		else if (*format != '%')
-		{
+		else
 			write(1, format, 1);
-		}
-		format++;
 		print_char++;
-		va_end(arg_list);
+		format++;
 	}
+	va_end(arg_list);
 	return (print_char);
 }
